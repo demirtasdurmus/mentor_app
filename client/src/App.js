8e27b6ff784af2e4b89f565dc14c095a34f1766c
Durmuş <demirtasdurmus@gmail.com>
2022-03-19 13:19:06 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import './assets/style/App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import PrivateRoute from './routers/PrivateRoute';
+//import PrivateRoute from './routers/PrivateRoute';
 import PublicRoute from './routers/PublicRoute';
 import UserSkills from "./pages/UserSkills";
 import SignIn from "./pages/SignIn";
